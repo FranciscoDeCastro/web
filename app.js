4a01c16d2a841d8dcb55abb2b031119285926933
Francisco de Castro <franciscodc2002@gmail.com>
2022-10-21 18:31:40 -0300

    added 'fotoCarnet' and information

--- app.js
+++ app.js
@@ -11,3 +11,15 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+function getMonthDifference() {
+    startDate = new Date('2022-06-8')
+    endDate = new Date()
+    return (
+        endDate.getMonth() -
+        startDate.getMonth() +
+        12 * (endDate.getFullYear() - startDate.getFullYear()) + "+"
+    );
+  }
+
+  document.getElementById("cantMesesExperiencia").innerHTML = getMonthDifference();
