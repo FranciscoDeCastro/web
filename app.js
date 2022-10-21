9d165c1df3c14329ca13c476d4a0013202c1fb19
Francisco de Castro <franciscodc2002@gmail.com>
2022-10-21 18:45:43 -0300

    5 pdfs uploaded to files folder

--- app.js
+++ app.js
@@ -10,4 +10,16 @@
     document.querySelector(".theme-btn").addEventListener("click", () => {
         document.body.classList.toggle("light-mode");
     })
-})();z
+})();
+
+function getMonthDifference() {
+    startDate = new Date('2022-06-8')
+    endDate = new Date()
+    return (
+        endDate.getMonth() -
+        startDate.getMonth() +
+        12 * (endDate.getFullYear() - startDate.getFullYear()) + "+"
+    );
+  }
+
+  document.getElementById("cantMesesExperiencia").innerHTML = getMonthDifference();
