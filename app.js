7bd5a73fd68cdb8d27a510b0bc07678c040e3f28
Francisco de Castro <franciscodc2002@gmail.com>
2022-10-22 17:20:50 -0300

    fix progress bars

--- app.js
+++ app.js
@@ -43,18 +43,3 @@ function download(downloadLink, downloadName){
   link.click();
   document.body.removeChild(link);    
 }
-
-function sendEmail() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "sender@email_address.com",
-    Password: "Enter your password",
-    To: 'receiver@email_address.com',
-    From: "sender@email_address.com",
-    Subject: "Sending Email using javascript",
-    Body: "Well that was easy!!",
-  })
-    .then(function (message) {
-      alert("mail sent successfully")
-    });
-}
